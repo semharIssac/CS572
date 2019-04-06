15c5de9ef699babb9a44fe3cbff70a66affe2425
semharIssac <semharweldegiorgis@gmail.com>
2019-04-05 22:41:13 -0500

    Homework 5

--- homework_05/app.js
+++ homework_05/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const axios = require('axios');
 var app = express();
 
+app.enable('trust proxy');
+app.enable('strict routing');
+app.set('x-powered-by', false);
 
+//ms('1d');
 app.get('/users',async(req, res)=>{
   try {
     const response = await axios.get('https://randomuser.me/api/?results=10');
+    res.links({
+      next: 'https://randomuser.me/api/?results=10',
+      last: 'https://randomuser.me/api/?results=10'
+    });
     //console.log(response);
-    res.send(JSON.stringify(response.data.results))
+    res.send(response.data.results)
   } catch (error) {
     console.error(error);
   }
