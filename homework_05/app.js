d12723f696aaff9e9b1cc67983af83abe4259b3a
semharIssac <semharweldegiorgis@gmail.com>
2019-04-08 21:04:52 -0500

    homework 6

--- homework_05/app.js
+++ homework_05/app.js
@@ -2,23 +2,28 @@ const express = require('express');
 const axios = require('axios');
 var app = express();
 
-app.enable('trust proxy');
-app.enable('strict routing');
-app.set('x-powered-by', false);
+ app.enable('trust proxy');
+ app.enable('strict routing');
+ app.set('x-powered-by', false);
+ app.enable('case sensitive routing');
+ 
 
 //ms('1d');
 app.get('/users',async(req, res)=>{
+  res.set('Cache-Control', 'public, max-age=31557600');
   try {
     const response = await axios.get('https://randomuser.me/api/?results=10');
-    res.links({
-      next: 'https://randomuser.me/api/?results=10',
-      last: 'https://randomuser.me/api/?results=10'
-    });
+    // res.links({
+    //   next: 'https://randomuser.me/api/?results=10',
+    //   last: 'https://randomuser.me/api/?results=10'
+    // });
     //console.log(response);
     res.send(response.data.results)
   } catch (error) {
     console.error(error);
   }
+  
+  res.json(res.JSONResponse)
 })
 .listen(4000, ()=>(console.log('Done!')))
 
